5d2258a9186faa9f1b1df504a7ffc08adf0b56a8
Simon Hafner <hafnersimon@gmail.com>
2013-01-14 19:31:27 -0600

    IntegerPattern should accept negative number
    Important in the factorial exercise so the correct error message is
    printed.

--- homework/hw1/addRange.scala
+++ homework/hw1/addRange.scala
@@ -1,7 +1,7 @@
 // Get the arguments from the command line and check there are two of
 // them and that they are all integers. (Don't change the next block
 // of code.)
-val IntegerPattern = """\d+""".r
+val IntegerPattern = """-?\d+""".r
 if (args.length != 2 || args.exists(arg => !IntegerPattern.pattern.matcher(arg).matches)) {
   println("Incorrect arguments to variables.scala. Please provide two integers.")
   System.exit(0)
